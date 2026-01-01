522e1d3259ad59a83239ce220826db3cceb771f0
bthntaylan1 <batuhantaylanmarun@gmail.com>
2026-01-01 23:24:28 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
-"""
-Ana çalışma dosyası
-Öğrenciler bu dosyayı çalıştıracaktır.
-"""
+from src.io_utils import load_signal_csv
+from src.signal_tools import sampling_frequency, moving_average
+from src.analysis import basic_analysis
+from src.plotting import plot_signals
+
 
 def main():
-    # TODO:
-    # 1. CSV dosyasını oku
-    # 2. örnekleme frekansını hesapla
-    # 3. temel istatistikleri yazdır
-    # 4. hareketli ortalama filtresi uygula
-    # 5. sonucu çizdir
-    pass
+    t, x = load_signal_csv("data/sample_signal.csv")
+
+    fs = sampling_frequency(t)
+    print(fs)
+
+    mean, std, rms, min_val, max_val = basic_analysis(x)
+    print(mean, std, rms, min_val, max_val)
+
+    x_filtered = moving_average(x, window_size=5)
+
+    plot_signals(
+        t,
+        x,
+        x_filtered,
+        "report/figures/time_signal.png"
+    )
 
 
 if __name__ == "__main__":
